3037c5b6d5bfc6a1904bd45e291b3648a1653fa2
trongthanhqvo <trongthanhqvo@github.com>
2021-09-05 08:56:30 +0700

    update sample hello word

--- appengine/standard_python3/hello_world/main_test.py
+++ appengine/standard_python3/hello_world/main_test.py
@@ -21,4 +21,4 @@ def test_index():
 
     r = client.get('/')
     assert r.status_code == 200
-    assert 'Hello World' in r.data.decode('utf-8')
+    assert 'Nguyễn Trọng Thanh! Xin chào thầy Cấn Đức Điệp!' in r.data.decode('utf-8')
